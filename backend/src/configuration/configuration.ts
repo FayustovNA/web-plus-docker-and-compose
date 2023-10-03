b4877c1dd05b014abef9f8d1eb1c7e93676316ee
Nikolay Fayustov <n.a.fayustov@gmail.com>
2023-10-03 20:51:46 +0300

    psq

--- backend/src/configuration/configuration.ts
+++ backend/src/configuration/configuration.ts
@@ -1,10 +1,10 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
-    db_host: process.env.DATABASE__HOST || 'localhost',
-    db_port: parseInt(process.env.DATABASE_PORT, 10),
-    db_username: process.env.DATABASE_USERNAME || 'student',
-    db_password: process.env.DATABASE_PASSWORD || 'student',
-    database: process.env.DATABASE_NAME || 'kupipodariday',
+    db_host: process.env.POSTGRES_HOST || 'postgres',
+    db_port: parseInt(process.env.POSTGRES_PORT, 10),
+    db_username: process.env.POSTGRES_USERNAME || 'student',
+    db_password: process.env.POSTGRES_PASSWORD || 'student',
+    database: process.env.POSTGRES_NAME || 'kupipodariday',
     secret_jwt: process.env.SECRET_JWT || 'VerySecret',
     expire_jwt: process.env.EXPIRE_JWT || 86000,
 });
